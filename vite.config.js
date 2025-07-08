916f1a266f45617e8ac33756f3f6fb09844664d6
Akash <59678417+AkashTemburnikar@users.noreply.github.com>
2025-07-07 19:57:20 -0500

    updated config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Akash-MyPortfolio/',
+  base: '/Akash-Portfolio/',
   plugins: [react()],
 })
